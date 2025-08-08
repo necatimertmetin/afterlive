9bc6dbb782bab2e200d1154a2238c1d2ad967fa4
mert metin <necatimertmetin@gmail.com>
2025-08-08 22:28:58 +0300

    base path defined

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function AppContent() {
   return (
     <CacheProvider value={cache}>
       <CssBaseline />
-      <BrowserRouter>
+      <BrowserRouter basename="/afterlife">
         <Routes>
           <Route element={<PageLayout />}>
             {ROUTES.map((route) => (
