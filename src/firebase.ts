308e759f8301dbc52a18e1f15acfebfe7f56e6c1
mert metin <mert.metin@vngrs.com>
2025-08-23 08:15:57 +0300

    events json transported to github

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,4 +22,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
-export const db = getDatabase(app);
--- src/hooks/useEvents.tsx
+++ src/hooks/useEvents.tsx
@@ -1,27 +1,26 @@
 import { useState, useEffect } from "react";
 import type { Event } from "../types/Event";
-import { db } from "../firebase"; // firebase.ts yolu
-import { ref, onValue } from "firebase/database";
 
 export function useEvents() {
   const [events, setEvents] = useState<Event[]>([]);
 
   useEffect(() => {
-    const eventsRef = ref(db); // Firebase DB'deki 'events' node'u
-    const unsubscribe = onValue(eventsRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
+    // jsDelivr CDN üzerinden JSON dosyanın URL'si
+    const url =
+      "https://cdn.jsdelivr.net/gh/necatimertmetin/afterlive@main/event.json";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
         // Object -> Array dönüştür
         const eventsArray: Event[] = Object.values(data);
         setEvents(eventsArray);
-      } else {
+      })
+      .catch((err) => {
+        console.error("Failed to fetch events:", err);
         setEvents([]);
-      }
-    });
-    console.log(events);
-    // Cleanup
-    return () => unsubscribe();
-  }, [events]);
+      });
+  }, []);
 
   return events;
 }
